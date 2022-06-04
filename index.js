496826d5617836cc6f9843b7877ffe1babdf6704
Sopan Setiawan <setiawan.sopan@gmail.com>
2022-06-05 00:48:29 +0700

    feat : commit ke 1

--- index.js
+++ index.js
@@ -6,15 +6,30 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (data, sortByFunc) => {
+  const dataTerurut = sortByFunc(data);
+  const dataBernomor = [];
+
+  for (let i=0; i<dataTerurut.length; i++) {
+    dataBernomor.push(i+1 +'. '+ dataTerurut[i]);
+  }
+
+  return dataBernomor
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+  const dataUrutAsc = data.sort();
+  return dataUrutAsc;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (data) => {
+  const dataUrutDsc = data.sort().reverse();
+  return dataUrutDsc;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
